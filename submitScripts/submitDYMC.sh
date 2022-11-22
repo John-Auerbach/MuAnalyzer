6878302ecf135cf89827d2b92c02862bf06e98c9
= <michael.martin.r@cern.ch>
2022-11-22 15:36:49 -0600

    Added submit script

---
+++ submitScripts/submitDYMC.sh
@@ -0,0 +1,3 @@
+#perl condor_filelist.perl python/ConfFile_MuAnalyzer_cfg.py datafiles/DYJets_mumuFilter_test.txt isMC=True --prodSpace /data/cmszfs1/user/revering/ZmmSim --batch 10 --jobname ZmmSim_testBarrelCheck
+perl condor_filelist.perl python/ConfFile_MuAnalyzer_cfg.py datafiles/DYJets_mumuFilter_combined.txt isMC=True --prodSpace /data/cmszfs1/user/revering/ZmmSim --batch 10 --jobname ZmmSim_testBarrelCheck
+perl condor_filelist.perl python/ConfFile_MuAnalyzer_cfg.py datafiles/DYJets_mumuFilter_part2.txt isMC=True --prodSpace /data/cmszfs1/user/revering/ZmmSim --batch 10 --jobname ZmmSim_testBarrelCheck_2
